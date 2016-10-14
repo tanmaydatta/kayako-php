ecd371ac309b994c0d79fa45b0e018b9ff0814ff
Tanmay Datta <tanmaydatta@gmail.com>
2016-10-14 23:35:18 +0530

    final

--- src/Models/Tweets.php
+++ src/Models/Tweets.php
@@ -1,34 +1,26 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: shivangi
- * Date: 10/14/16
- * Time: 3:32 AM
- */
+
+namespace Kayako\Model;
 
 class Tweets
 {
-    public function convertResultToJson($result)
-    {
-        $jsonData = json_decode($result);
-        $data = $jsonData->{'statuses'};
-        return $data;
-    }
-
     public function extractRetweetedTweets($result)
-    {   $retweetedList=null;
-
-        $tweetList = $this->convertResultToJson($result);
+    {   
+        $response = [];
+        $response["tweets"] = null;
+        $response["status"] = "success";
+        $response["next_url"] = $result->search_metadata->next_results;
+        $tweetList = $result->statuses;
         foreach ($tweetList as $tweet)
         {
-            $retweet_count = $tweet->{'retweet_count'};
-            if(json_encode($retweet_count)>=1)
+            $retweet_count = $tweet->retweet_count;
+            if($retweet_count>=1)
             {
-                $retweetedList[]=$tweet;
+                $response["tweets"][]=$tweet;
+                $ret[]=$tweet;
             }
         }
-
-        return $retweetedList;
+        return json_encode($response);
     }
 
 }
--- src/NetworkCalls/TwitterApiCalls.php
+++ src/NetworkCalls/TwitterApiCalls.php
@@ -1,60 +1,24 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: shivangi
- * Date: 10/14/16
- * Time: 4:05 AM
- */
 
 namespace Kayako\NetworkCalls;
 use Kayako\Authorization\AuthorizationCredentials;
+use Abraham\TwitterOAuth\TwitterOAuth;
 
 class TwitterApiCalls
 {
 
-    protected $SEARCH_API = "https://api.twitter.com/1.1/search/tweets.json?q=%23";
-    protected $curl;
-    protected $authorizationCredentials;
-    protected $hashTag;
+    protected $SEARCH_API = "search/tweets";
+    protected $connection;
 
     public function __construct()
     {
-        $this->curl = curl_init();
         $oauthCredentialObj = new AuthorizationCredentials();
-        $this->authorizationCredentials = $oauthCredentialObj->getOauthToken();
+        $this->connection = $oauthCredentialObj->getConnection();
     }
-    public function executeSearchApi($numberOfWeeks)
+    public function executeSearchApi($param)
     {
-        $this->hashTag = 'custserv';
-
-        $dateParam = $this->calculateDate($numberOfWeeks);
-        $newUrl=$this->SEARCH_API.$this->hashTag;
-        // $newUrl=$this->SEARCH_API.$this->hashTag."&until=".$dateParam;
-
-
-        //echo $newUrl;
-        curl_setopt_array($this->curl, array(
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => $newUrl,
-            CURLOPT_HTTPHEADER => array($this->authorizationCredentials, "Content-Type: application/x-www-form-urlencoded"),
-            CURLOPT_VERBOSE => 1,
-        ));
-        $response = curl_exec($this->curl);
-       
+        $response = $this->connection->get($this->SEARCH_API, ["q" => "#".$param["q"], "max_id" => $param["max_id"], "count" => "50"]);
         return $response;
-    }
-
-    public function calculateDate($numberOfWeeks)
-    {
-        $currentTime = time();
-        $secondInAweek = 604800;
-        $xWeeks = $secondInAweek * $numberOfWeeks;
-
-        $requiredTimeStamp = $currentTime - $xWeeks;
-        date_default_timezone_set('UTC');
-        $date = date('Y-m-d', $requiredTimeStamp);
-
-        return $date;
     }
 
 }
