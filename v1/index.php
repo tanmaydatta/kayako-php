f3b6333502196effcf8f1093640a094150bd935c
Tanmay Datta <tanmaydatta@gmail.com>
2016-10-14 20:16:42 +0530

    procfile

--- v1/index.php
+++ v1/index.php
@@ -1,17 +1,18 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+// use \Psr\Http\Message\ServerRequestInterface as Request;
+// use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../vendor/autoload.php';
+// require '../vendor/autoload.php';
 
-$app = new \Slim\App;
-$app->get('/hello', function (Request $request, Response $response) {
-    // $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello");
+// $app = new \Slim\App;
+// $app->get('/hello', function (Request $request, Response $response) {
+//     // $name = $request->getAttribute('name');
+//     $response->getBody()->write("Hello");
 
-    return $response;
-});
-$app->run();
+//     return $response;
+// });
+// $app->run();
+echo "hello";
 // <!-- $app = new \Slim\Slim(); -->
 
 ?>
